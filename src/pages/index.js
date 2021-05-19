c7c34ccfcbbbfc945882159708b84344b9a0360e
akshat <akshat0199@gmail.com>
2021-05-19 22:57:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
             <span className="text-primary"> Sahu</span>
           </h1>
           <div className="subheading mb-5">
-            Malad-east, Mumbai, India . +(91) 9167022163. akshat0199@gmail.com
+            Malad-east, Mumbai, India .+(91) 9167022163. akshat0199@gmail.com
           </div>
           <p className="lead mb-5">
             I am experienced in leveraging agile frameworks to provide a robust
@@ -36,6 +36,40 @@ const IndexPage = () => (
                 </a>
               );
             })}
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="education"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Education</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">University of Colorado Boulder</h3>
+              <div className="subheading mb-3">Bachelor of Science</div>
+              <div>Computer Science - Web Development Track</div>
+              <p>GPA: 3.23</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">August 2006 - May 2010</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+            <div className="resume-content">
+              <h3 className="mb-0">James Buchanan High School</h3>
+              <div className="subheading mb-3">Technology Magnet Program</div>
+              <p>GPA: 3.56</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">August 2002 - May 2006</span>
+            </div>
           </div>
         </div>
       </section>
